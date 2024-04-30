3baddc0bbc9ee9cfeadeabc3d9b9c6fade7c5ff4
choiyun9yu <sw22tapple@gmail.com>
2024-04-30 11:07:31 +0900

    update: flask

--- Back-end/RESTful_API/Flask/app/__init__.py
+++ Back-end/RESTful_API/Flask/app/__init__.py
@@ -1,16 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
-
-from config import Config
-from .models import mongo
 from .routes import api_blueprint
+
+# from config import Config
+# from .models import mongo
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
-    app.config.from_object(Config)
-    mongo.init_app(app)
     app.register_blueprint(api_blueprint)
+    # app.config.from_object(Config)
+    # mongo.init_app(app)
 
     return app
 
--- Back-end/RESTful_API/Flask/app/routes.py
+++ Back-end/RESTful_API/Flask/app/routes.py
@@ -1,6 +1,6 @@
 from flask import jsonify, Blueprint, request
 
-from .models import MongoDB, MySQL, Oracle
+# from .models import MongoDB, MySQL, Oracle
 
 api_blueprint = Blueprint('api', __name__)
 
@@ -8,23 +8,23 @@ api_blueprint = Blueprint('api', __name__)
 def index():
     ip = request.remote_addr    # 클라이언트 ip
 
-    return '<h1> Welcome to Yun9yu\'s Flask Legacy </h1>'
+    return '<h1> Welcome Flask Legacy </h1>'
 
-@api_blueprint.route('/api/mongodb', methods=['GET', 'POST'])
-def read_monogdb():
-    if request.method == 'GET':
-        requset.get_json()
-        fieldName1 = request.args['fieldName1']
-        fieldName2 = request.args['fieldName2']
-        fieldData = MongoDB.get_and(fieldName1, fieldName2)
-        return jsonify({"fieldName1": fieldName1, "fieldName2": fieldName2})
-
-    elif request.method == 'POST':
-        form_data = request.form
-        parsed_data = dict(form_data)
-        periodFrom, periodTo = (parsed_data['periodFrom'], parsed_data['periodTo'])
-        period = Drone.get_period(periodFrom, periodTo)
-        return jsonify({"period": period})
+# @api_blueprint.route('/api/mongodb', methods=['GET', 'POST'])
+# def read_monogdb():
+#     if request.method == 'GET':
+#         requset.get_json()
+#         fieldName1 = request.args['fieldName1']
+#         fieldName2 = request.args['fieldName2']
+#         fieldData = MongoDB.get_and(fieldName1, fieldName2)
+#         return jsonify({"fieldName1": fieldName1, "fieldName2": fieldName2})
+#
+#     elif request.method == 'POST':
+#         form_data = request.form
+#         parsed_data = dict(form_data)
+#         periodFrom, periodTo = (parsed_data['periodFrom'], parsed_data['periodTo'])
+#         period = Drone.get_period(periodFrom, periodTo)
+#         return jsonify({"period": period})
 
 
 
